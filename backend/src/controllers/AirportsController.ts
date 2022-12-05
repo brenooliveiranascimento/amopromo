a593f78a5c91c4cb9c237d7a7a0e3e83b183e026
Breno Nascimento <satsukebare23@gmail.com>
2022-12-05 15:19:27 -0300

    add get aiurport by iata controller

--- backend/src/controllers/AirportsController.ts
+++ backend/src/controllers/AirportsController.ts
@@ -4,6 +4,7 @@ import AirportModel from '../database/models/AirportModel';
 interface IAirportService {
   handleAirportStatus: (id: number, currStatus: boolean) => Promise<void>;
   getAll: () => Promise<AirportModel[]>;
+  getByIata: (iata: string) => Promise<AirportModel>;
 }
 
 export default class AirportController {
@@ -14,12 +15,18 @@ export default class AirportController {
     const { id, currStatus } = req.params;
 
     await this._airportService.handleAirportStatus(Number(id), JSON.parse(currStatus));
-    return res.status(201).json({ message: 'airport status updated with success!!' })
+    return res.status(201).json({ message: 'airport status updated with success!!' });
   }
 
   async getAll(_req: Request, res: Response) {
     const airports = await this._airportService.getAll();
     return res.status(201).json({ message: airports });
+  }
+
+  async getByIata(req: Request, res: Response) {
+    const { iata } = req.params;
+    const airport = await this._airportService.getByIata(iata);
+    return res.status(200).json({ message: airport });
   }
 
 }

e7be79812e88b379642e49f318e224f5d7faa75c
Breno Nascimento <satsukebare23@gmail.com>
2022-12-06 18:27:26 -0300

    init mount options

--- backend/src/service/TravelsService.ts
+++ backend/src/service/TravelsService.ts
@@ -1,39 +1,56 @@
 import { baseHeader } from "../utils/airportConnection";
 import AirportModel from "../database/models/AirportModel";
-import { mockAirlineApi } from "../utils/mockAirlineConnection";
+import { mockAirlineApi, requestTravel } from "../utils/mockAirlineConnection";
 import AirportsService from "./AirportsService";
 import { IMountTravelParams } from "../interfaces/TravelInterfaces";
 import CustomError from "../utils/StatusError";
+import { ErrorMap } from "../utils/errorMap";
 
 export default class TravelService {
   constructor(private airportsService = new AirportsService()) {}
+
+
+  mountOptions() {
+    
+  }
+
   private async getTravel(travelParams: IMountTravelParams) {
-    const { arrival, depure, exitDate, returnDate } = travelParams;
+    const { exitDate, returnDate } = travelParams;
+
     const exitAndReturnDate = Promise.all([exitDate, returnDate]
       .map(async (currDate: Date | string, index: number) => {
-      const { data } = await mockAirlineApi.get(`/${arrival}/${depure}/${currDate}`, baseHeader);
-      if(!index) {
-        const mountData = { type: 'Exit', ...data }
-        return mountData
-      }
-      const mountData = { type: 'return', ...data }
-      return mountData
+      const data = await requestTravel(travelParams, currDate);
+      if(!index) return { type: 'Exit', ...data };
+      return { type: 'return', ...data };
     }))
-    return exitAndReturnDate;
+    const mountOptions = await (await exitAndReturnDate).map((currTravel) => this.mountOptions());
   }
 
   private async checkAirportsExist(airports: string[]):Promise<boolean> {
     const getAirports = await Promise
       .all(airports.map(async(currAirport: string) => await this.airportsService.getByIata(currAirport)));
-    const validate = getAirports.every((currAirport: AirportModel) => currAirport)
+
+    const validate = getAirports.every((currAirport: AirportModel) => currAirport);
+
     if(validate) return true;
-    throw new CustomError("Invalid Airport!", 404);
+    throw new CustomError(ErrorMap.INVALID_AIRPORT, 404);
+  }
+
+  private async travelValidations(travelParams: IMountTravelParams): Promise<void> {
+    const { arrival, depure, exitDate, returnDate } = travelParams;
+
+    await this.checkAirportsExist([arrival, depure]);
+
+    if(new Date(exitDate) > new Date(returnDate)) throw new CustomError(ErrorMap.RETURN_LESS, 404);
+    if(new Date(exitDate) > new Date()) throw new CustomError(ErrorMap.EXIT_NOW, 404);
+    if(arrival === depure) throw new CustomError(ErrorMap.EQUAL_DESTINATIONS, 404);
   }
 
   async mountTravel(travelParams: IMountTravelParams): Promise<any> {
-    const { arrival, depure } = travelParams;
-    await this.checkAirportsExist([arrival, depure]);
+    await this.travelValidations(travelParams);
+
     const travel = await this.getTravel(travelParams);
+
     return travel
   }
 }
--- backend/src/utils/mockAirlineConnection.ts
+++ backend/src/utils/mockAirlineConnection.ts
@@ -1,5 +1,14 @@
 import axios from "axios"
+import { IMountTravelParams } from "../interfaces/TravelInterfaces";
+import { baseHeader } from "./airportConnection";
 
 export const mockAirlineApi = axios.create({
   baseURL: 'http://stub.2xt.com.br/air/search/pzrvlDwoCwlzrWJmOzviqvOWtm4dkvuc'
 })
+
+export const requestTravel = async (travelParams: IMountTravelParams, currDate: Date | string) => {
+  const { arrival, depure } = travelParams;
+  const { data } = await mockAirlineApi
+    .get(`/${arrival}/${depure}/${currDate}`, baseHeader);
+  return data
+} 

3eb0f2fa209c6dafc7b66f9237a2bcecfa0306d5
Breno Nascimento <satsukebare23@gmail.com>
2022-12-07 19:53:46 -0300

    create logincard

---
+++ frontend/src/components/ui/LoginCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LoginCard() {
+  return(
+    <section>
+      <input placeholder="email" />
+      <input placeholder="password" />
+      <button>Entrar</button>
+    </section>
+  )
+}
---
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function LoginPage() {
+  return (
+    <main className='main_container'>
+      <h1>Login</h1>
+    </main>
+  )
+}
